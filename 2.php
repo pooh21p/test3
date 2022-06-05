336788208d5d4801f04e0397cf40de21ac44ce58
pooh21p <uchkurov@mail.ru>
2022-06-05 12:04:54 +0300

    Step 3

--- 2.php
+++ 2.php
@@ -1,42 +1,40 @@
-<CENTER><TEXTAREA ROWS=8 style="width:90%;color:green;">
-Задание 2
-На странице https://alef.im есть какое-то количество ссылок. Анкором ссылки называется текст, находящийся между тегами <a> и </a>. Посчитайте сколько раз встречается буква "з" внутри анкоров ссылок на этой странице. Регистр буквы не учитывайте, считайте все вхождения этой буквы: как в прописном, так и строчном видах.
-</TEXTAREA></CENTER> <BR> 
+<CENTER><TEXTAREA ROWS = "8" style = "width:90%;color:green;"> 
+Задание 2.
+На странице https://alef.im есть какое-то количество ссылок. Анкором ссылки называется текст, находящийся между тегами <a> и </a>. Посчитайте сколько раз встречается буква "з" внутри анкоров ссылок на этой странице. Регистр буквы не учитывайте, считайте все вхождения этой буквы: как в прописном, так и строчном видах. 
+ </TEXTAREA></CENTER> <BR> 
 
 <?php
 
-$text=isset($_REQUEST['text']) ? $_REQUEST['text'] : '';
+    $text = isset($_REQUEST['text']) ? $_REQUEST['text'] : '';
 
-if(empty($text))
-    $text='з';
+    if (empty($text))
+        $text = 'з';
 
 ?>
-<form method="POST">
-Текст<br><input name="text" type="text" value="<?php echo $text; ?>"><BR>
-<input type="submit" value="calculate">
-</form>
+ <form method = "POST"> 
+Текст <br><input name = "text" type = "text" value = "<?php echo $text; ?>"><BR> 
+ <input type = "submit" value = "calculate"> 
+ </form> 
 <?php
 
-
-
-    $t=file_get_contents('https://alef.im');
+    $t = file_get_contents('https://alef.im');
   
-    $count=0;    
-    $p=0;
-    while(true){
-        $p1=stripos($t,'<a ',$p);
-        if($p1===false)
+    $count = 0;    
+    $p = 0;
+    while (true) {
+        $p1 = stripos($t,'<a ',$p);
+        if ($p1===false)
             break;
-        $p=stripos($t,'</a>',$p1);
-        $an=substr($t,$p1,$p-$p1);
+        $p = stripos($t,'</a>',$p1);
+        $an = substr($t,$p1,$p - $p1);
         
-        $p1=stripos($an,'>');
-        $an=substr($an,$p1+1);
+        $p1 = stripos($an,'>');
+        $an = substr($an,$p1 + 1);
 
-        $an=substr($an,$p1+1);
+        $an = substr($an,$p1 + 1);
 
         $count += substr_count(mb_strtolower($an), mb_strtolower($text));
-        
+		
     }
   
-echo "result=".$count;
+echo "result = " . $count;
--- 3.php
+++ 3.php
@@ -1,47 +1,46 @@
-<CENTER><TEXTAREA ROWS=8 style="width:90%;color:green;">
-Задание 3
-Возьмите все числа от 1 до 10000 (включительно). Выбросьте из этой последовательности все числа, где встречаются последовательности из трех последовательно восходящих цифр (например 012 или 678). Найдите сумму оставшихся чисел.
-</TEXTAREA></CENTER> <BR> 
+<CENTER><TEXTAREA ROWS = "8" style = "width:90%;color:green;"> 
+Задание 3.
+Возьмите все числа от 1 до 10000 (включительно). Выбросьте из этой последовательности все числа, где встречаются последовательности из трех последовательно восходящих цифр (например 012 или 678). Найдите сумму оставшихся чисел. 
+ </TEXTAREA></CENTER> <BR> 
 
 <?php
 
-$n1=isset($_REQUEST['n1']) ? $_REQUEST['n1'] : 1;
-$n2=isset($_REQUEST['n2']) ? $_REQUEST['n2'] : 10000;
-    
-
+    $n1 = isset($_REQUEST['n1']) ? $_REQUEST['n1'] : 1;
+    $n2 = isset($_REQUEST['n2']) ? $_REQUEST['n2'] : 10000;
 ?>
-<form method="POST">
-от<br><input name="n1" type="text" value="<?php echo $n1; ?>"><BR>
-до<br><input name="n2" type="text" value="<?php echo $n2; ?>"><BR>
-<input type="submit" value="calculate">
-</form>
+ <form method = "POST"> 
+от <br><input name = "n1" type = "text" value = "<?php echo $n1; ?>"><BR> 
+до <br><input name = "n2" type = "text" value = "<?php echo $n2; ?>"><BR> 
+ <input type = "submit" value = "calculate"> 
+ </form> 
 <?php
 
-	function has_sequence($n){
+    function has_sequence($n)
+    {
         
-        $c=0;
-        $val=0;
-        while($n>0){
-            $d=$n%10;
-            $n=floor($n/10);
+        $c = 0;
+        $val = 0;
+        while ($n> 0) {
+            $d = $n%10;
+            $n = floor($n / 10);
             
-            if($d!=$val-1){
-                $c=1;
+            if ($d != $val - 1) {
+                $c = 1;
             }else{
-                $c++;
+                $c ++ ;
             }
-            $val=$d;
-            if($c>=3)
+            $val = $d;
+            if ($c >= 3)
                 return true;
         }
-		return false;
-	}
+        return false;
+    }
 
 
-    $sum=0;
-    for($n=$n1;$n<=$n2;$n++){
-        if(!has_sequence($n))
+    $sum = 0;
+    for ($n = $n1; $n <= $n2; $n ++ ) {
+        if (!has_sequence($n))
             $sum += $n;         
     }
   
-echo "result=".$sum;
+echo "result = " . $sum;
